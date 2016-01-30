72a2662a078ae85600671bc721c3f403554ca017
dragifon <dragifon@aim.com>
2016-01-30 14:00:10 -0500

    tylers-fault

--- TrialByFire/Assets/Code/DoorTrigger.cs
+++ TrialByFire/Assets/Code/DoorTrigger.cs
@@ -3,9 +3,14 @@ using System.Collections;
 
 public class DoorTrigger : MonoBehaviour {
 
+	void OnTriggerEnter2D(Collider2D other) {
+		//Debug.Log("collision");
+		//Destroy(other.gameObject);
+		Player.Transform.right += 10.0f;
+	}
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
--- TrialByFire/Assets/Code/PlayerController.cs
+++ TrialByFire/Assets/Code/PlayerController.cs
@@ -12,7 +12,7 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float mod = speed * Time.deltaTime;
-		GetComponent<Rigidbody2D>().AddForce(new Vector3(Input.GetAxis("Horizontal") * mod, Input.GetAxis("Vertical") * mod, 0f));
+		GetComponent<Rigidbody2D>().velocity=new Vector2(Input.GetAxisRaw("Horizontal") * mod, Input.GetAxisRaw("Vertical") * mod);
 		//transform.Translate(Input.GetAxis("Horizontal") * mod, Input.GetAxis("Vertical") * mod, 0f);
 		// this is a stupid comment
 	}

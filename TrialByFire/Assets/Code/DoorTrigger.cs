5881d775a6952b8bea612a7baf5ed88b838dbf4d
znickbakerz <znickbakerz@gmail.com>
2016-01-30 15:11:12 -0500

    SpawnerAndCollisions

--- TrialByFire/Assets/Code/DoorTrigger.cs
+++ TrialByFire/Assets/Code/DoorTrigger.cs
@@ -3,10 +3,15 @@ using System.Collections;
 
 public class DoorTrigger : MonoBehaviour {
 
+	public GameObject spawnPoint;
+	public Camera main;
+
 	void OnTriggerEnter2D(Collider2D other) {
 		//Debug.Log("collision");
 		//Destroy(other.gameObject);
-		Player.Transform.right += 10.0f;
+		//other.transform.Translate (new Vector2 (10.0f, 0f));
+		other.transform.position = spawnPoint.transform.position;
+		main.transform.Translate (new Vector2 (12.9f, 0f));
 	}
 	// Use this for initialization
 	void Start () {

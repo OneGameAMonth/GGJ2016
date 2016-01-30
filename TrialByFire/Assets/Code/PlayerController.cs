65e45ac63d0c7a2f0b2beb48b89f37b6163dfa22
Tyler Mitchell <tylermitchellisawesome@gmail.com>
2016-01-30 00:40:05 -0500

    init

---
+++ TrialByFire/Assets/Code/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 1000.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mod = speed * Time.deltaTime;
+		GetComponent<Rigidbody2D>().AddForce(new Vector3(Input.GetAxis("Horizontal") * mod, Input.GetAxis("Vertical") * mod, 0f));
+		//transform.Translate(Input.GetAxis("Horizontal") * mod, Input.GetAxis("Vertical") * mod, 0f);
+	}
+}

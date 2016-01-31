1ce36f2038b8a4ce3edb23690091d0f32c1132b9
Tyler Mitchell <tylermitchellisawesome@gmail.com>
2016-01-31 05:49:53 -0500

    Bunch of stuff

--- TrialByFire/Assets/Code/PlayerController.cs
+++ TrialByFire/Assets/Code/PlayerController.cs
@@ -2,31 +2,40 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
+	public Animator anim;
 	public float speed;
-	public bool isCarryingTorch = false;
+	public bool carryingTorch = false;
 	// Use this for initialization
 	void Start () {
-		
+		anim = gameObject.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float mod = speed * Time.deltaTime;
-		GetComponent<Rigidbody2D>().velocity=new Vector2(Input.GetAxisRaw("Horizontal") * mod, Input.GetAxisRaw("Vertical") * mod);
+		float horizontal = Input.GetAxisRaw("Horizontal");
+		float vertical = Input.GetAxisRaw("Vertical");
+
+		if(vertical == 1f ){ anim.SetInteger("Direction", 3); }//up
+		else if(vertical == -1f){ anim.SetInteger("Direction", 1); }//down
+		else if(horizontal == 1f ){ anim.SetInteger("Direction", 2); }//right
+		else if(horizontal == -1f ){ anim.SetInteger("Direction", 4); }//left
+		GetComponent<Rigidbody2D>().velocity=new Vector2(horizontal * mod, vertical * mod);
 		//transform.Translate(Input.GetAxis("Horizontal") * mod, Input.GetAxis("Vertical") * mod, 0f);
 		// this is a stupid comment
 	}
 
 	void OnCollisionEnter2D(Collision2D coll) {
-		if( coll.gameObject.name == "Torch2" ){
+		GameObject gObj = coll.gameObject;
+		if( gObj.name == "Torch" ){
 			Destroy(coll.gameObject);
-			isCarryingTorch = true;
+			carryingTorch = true;
 		}
-		if( coll.gameObject.name == "Bonfire" && isCarryingTorch == true){
+		if( gObj.name == "Bonfire" && carryingTorch == true){
 			//swap the sprite on the bonfire to the lit sprite
 			//gameObject.getComponent<Renderer>()
-			isCarryingTorch = false;
+			gObj.GetComponent<BonfireController>().Light();
+			carryingTorch = false;
 		}
 	}
 }
---
+++ TrialByFire/Assets/Code/Tile.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+	public Sprite red1;
+	public Sprite red2;
+	public Sprite red3;
+
+	private int tileNum;
+	public bool isTarget;
+
+	public SpriteRenderer renderer;
+	private TileGenerator gen;
+	// Use this for initialization
+	void Start () {
+		gen = gameObject.GetComponentInParent<TileGenerator>();
+		//renderer = gameObject.GetComponentInParent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D( Collider2D other ){
+		if(isTarget){
+			Debug.Log("Test");
+			if(other.gameObject.name == "Player"){
+				Debug.Log("Player has touched target tile");
+				gen.TargetTileTouched();
+			}
+		}
+	}
+	public void SetTileNum( int n ){ tileNum = n; }
+	public void IsTargetTile( bool v ){ isTarget = v; }
+	public void SetTileType( int whichSprite){ //takes an int representing which sprite to show in the renderer
+		if(whichSprite > 0 || whichSprite < 4){
+			switch(whichSprite){
+				case 1: renderer.sprite = red1; break;
+				case 2: renderer.sprite = red2; break;
+				case 3: renderer.sprite = red3; break;
+				default: break;
+			}
+		}
+		else{
+			Debug.Log("Invalid Sprite number sent to SetTileType!" + whichSprite.ToString());
+			return;
+		}
+	}
+}

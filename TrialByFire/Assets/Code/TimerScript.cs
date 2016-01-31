32f54597029bde735ce0f6f11a5e05234d8cf2b5
znickbakerz <znickbakerz@gmail.com>
2016-01-31 05:55:11 -0500

    timer

---
+++ TrialByFire/Assets/Code/TimerScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerScript : MonoBehaviour {
+
+	float initialTime = 60f;
+	float timeLeft;
+	Vector2 originalScale;
+
+	// Use this for initialization
+	void Start () {
+		gameObject.tag = "RedBar";
+		originalScale = gameObject.transform.localScale;
+		timeLeft = initialTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timeLeft > 0) {
+		
+			timeLeft -= Time.deltaTime;
+			gameObject.GetComponent<RectTransform> ().localScale = new Vector2((timeLeft / initialTime), originalScale.y);
+		
+		}
+	}
+}

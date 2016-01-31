f493fd85d1f8a3e36db85cda52d8cc06d0960923
znickbakerz <znickbakerz@gmail.com>
2016-01-31 15:00:56 -0500

    losescreenandtimerto4minutes

--- TrialByFire/Assets/Code/TimerScript.cs
+++ TrialByFire/Assets/Code/TimerScript.cs
@@ -3,15 +3,17 @@ using System.Collections;
 
 public class TimerScript : MonoBehaviour {
 
-	float initialTime = 60f;
+	float initialTime = 240f;
 	float timeLeft;
 	Vector2 originalScale;
+	public GameObject lose;
 
 	// Use this for initialization
 	void Start () {
 		gameObject.tag = "RedBar";
 		originalScale = gameObject.transform.localScale;
 		timeLeft = initialTime;
+		lose.SetActive (false);
 	}
 	
 	// Update is called once per frame
@@ -19,8 +21,10 @@ public class TimerScript : MonoBehaviour {
 		if (timeLeft > 0) {
 		
 			timeLeft -= Time.deltaTime;
-			gameObject.GetComponent<RectTransform> ().localScale = new Vector2((timeLeft / initialTime), originalScale.y);
+			gameObject.GetComponent<RectTransform> ().localScale = new Vector2 ((timeLeft / initialTime), originalScale.y);
 		
+		} else {
+			lose.SetActive (true);
 		}
 	}
 }

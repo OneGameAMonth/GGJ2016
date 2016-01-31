a0e48fa57c765cd624a55a9a7a36484cfc64b122
dragifon <dragifon@aim.com>
2016-01-31 00:15:54 -0500

    - Did a little dance - Made a little love - Got down tonight - SSADS unlocks the stone for pressing the button - Tyler added Andy's audio - It is rad :beers: :dancer:

---
+++ TrialByFire/Assets/Code/MusicController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicController : MonoBehaviour {
+	public AudioSource intro;
+	public AudioSource loop;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(PlayMusic());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public IEnumerator PlayMusic(){
+		intro.Play();
+		yield return new WaitForSeconds(intro.clip.length);
+		loop.Play();
+	}
+}
